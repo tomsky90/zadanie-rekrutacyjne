85358edb2353f100775a4e2446c756c71e302140
Tomasz Kaszycki <kaszyckitomasz@interia.pl>
2024-03-28 07:22:22 +0000

    feat: create useFeatch hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
+import useFetch from "./hooks/useFetch";
 import "./App.css";
 
 function App() {
+  const { data, loading, error } = useFetch(
+    `/2.3/tags?order=desc&sort=popular&site=stackoverflow`
+  );
+
   return (
-    <Container maxWidth="sm">
-      <Typography
-        variant="h1"
-        style={{ fontSize: "32px", textAlign: "center" }}
-        sx={{ my: "40px" }}
-      >
-        Zadanie Rekrutacyjne
-      </Typography>
-    </Container>
+    <>
+      {console.log(data)}
+      <Container maxWidth="sm">
+        <Typography
+          variant="h1"
+          style={{ fontSize: "32px", textAlign: "center" }}
+          sx={{ my: "40px" }}
+        >
+          Zadanie Rekrutacyjne
+        </Typography>
+      </Container>
+    </>
   );
 }
 
---
+++ src/hooks/useFetch.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { makeRequest } from "../axiosSetup.tsx";
+
+export type Tags = {
+  name: string;
+  count: number;
+};
+
+const useFetch = (url: string) => {
+  const [data, setData] = useState<Tags[] | null>();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const res = await makeRequest.get(url);
+        setData(res.data);
+      } catch (err) {
+        setError(true);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useFetch;
